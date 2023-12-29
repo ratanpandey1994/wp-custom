d0c477db61f43d26a24e5cd3c1d5066da3619d7c
Ratan <ratan.pandey1994@gmail.com>
2023-12-29 09:46:08 +0530

    Update search.js

--- wp-content/themes/practice/js/search.js
+++ wp-content/themes/practice/js/search.js
@@ -43,17 +43,21 @@ class Search {
     }
 
     getResults() {
-        jQuery.getJSON(mainData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
-            jQuery.getJSON(mainData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val(), pages => {
-                var combinedResults = posts.concat(pages);
-                this.resultsDiv.html(`
+
+        jQuery.when(
+            jQuery.getJSON(mainData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()),
+            jQuery.getJSON(mainData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
+        ).then((posts, pages) => {
+            var combinedResults = posts[0].concat(pages[0]);
+            this.resultsDiv.html(`
                 <h2 class="search-overlay__section-title">General Information</h2>
                 ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No gereral information matches that search.</p>'}
                    ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join("")}                
                 ${combinedResults.length ? '</ul>' : ''}
             `);
-                this.isSpinnerVisible = false;
-            });
+            this.isSpinnerVisible = false;
+        }, () => {
+            this.resultsDiv.html('<p>Unexpected error; please try again.</p>')
         });
     }
 
